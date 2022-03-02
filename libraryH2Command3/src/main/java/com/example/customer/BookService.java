8904470fc36c7d200aa18b69706dd5465f542622
Albert.Profe <albertprofe@gmail.com>
2022-03-02 12:31:01 +0100

    commit #3 finished project

--- libraryH2Command3/src/main/java/com/example/customer/BookService.java
+++ libraryH2Command3/src/main/java/com/example/customer/BookService.java
@@ -1,5 +1,7 @@
 package com.example.customer;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -7,21 +9,92 @@ import org.springframework.stereotype.Service;
 public class BookService {
 	
 	@Autowired
-	BookRepository bookrepository;
+	BookRepository bookRepository;
 	
 	public Iterable<Book> findAll() {
 		
 		//System.out.println ("Books" + books);
 		
-		return bookrepository.findAll();
+		return bookRepository.findAll();
 	}
 	
 	public void save(Book book) {
 		
-		bookrepository.save(book);
+		bookRepository.save(book);
 		
 		
 	}
 	
-	
+	public String findAndDeleteById(String id) {
+
+		String response = "";
+		Optional<Book> bookFound = bookRepository.findById(id);
+
+		if (bookFound.isPresent()) {
+
+			bookRepository.delete(bookFound.get());
+			response += "book deleted";
+		} else {
+
+			response += "book not found";
+		}
+
+		return response;
+	}
+
+	public void deleteById(String id) {
+
+		// String response = "";
+
+		bookRepository.deleteById(id);
+
+	}
+
+	public String update(String id, Book book) {
+
+		String response = "";
+		Optional<Book> bookFound = bookRepository.findById(id);
+
+		if (bookFound.isPresent()) {
+
+			bookFound.get().setTitle(book.getTitle());
+			bookRepository.save(bookFound.get());
+			response += "book updated";
+
+		} else {
+			response += "book not found";
+		}
+
+		return response;
+
+	}
+
+	// other options
+	public long count() {
+
+		long quantity = bookRepository.count();
+
+		return quantity;
+	}
+
+	public boolean existsById(String id) {
+
+		boolean isBook = bookRepository.existsById(id);
+
+		return isBook;
+	}
+
+	public Long countByLastname(String author) {
+
+		long quantity = bookRepository.countByAuthor(author);
+
+		return quantity;
+	}
+
+	public void deleteByLastname(String author) {
+
+		bookRepository.deleteByAuthor(author);
+
+	}
+
 }
